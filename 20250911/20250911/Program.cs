e0760fd9e164f5251d49b866a83afe94369956a3
luluco1102 <adaddda12@gmail.com>
2025-09-11 09:41:43 +0900

    1

---
+++ 20250911/20250911/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _20250911
+{
+    internal class Program
+    {
+        static void Swap(ref int x,ref int y)
+        {
+            int temp = x;
+            x = y;
+            y = temp;
+        }
+
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 20;
+            Swap(ref x,ref y);
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+        }
+    }
+}
