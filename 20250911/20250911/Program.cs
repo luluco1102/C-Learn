354d493c0e5d64bd9f11eab938370ed491e47d9a
luluco1102 <adaddda12@gmail.com>
2025-09-11 15:43:07 +0900

    learn

--- 20250911/20250911/Program.cs
+++ 20250911/20250911/Program.cs
@@ -2,21 +2,69 @@
 {
     internal class Program
     {
-        static void Swap(ref int x,ref int y)
+        static void CountingStar(string s)
         {
-            int temp = x;
-            x = y;
-            y = temp;
+            
+
         }
 
         static void Main(string[] args)
         {
-            int x = 10;
-            int y = 20;
-            Swap(ref x,ref y);
+            int[,] arr = {
+                { 1, 2, 3},
+                { 4,5,6},
+                { 7,8,9}
+            };
 
-            Console.WriteLine(x);
-            Console.WriteLine(y);
+            for(int i=0; i<arr.GetLength(0); i++)
+            {
+                for(int j =0; j<arr.GetLength(1); j++)
+                {
+                    Console.Write(arr[j,i]);
+                    
+                }
+                Console.WriteLine();
+            }
+
+            Add(2, 2);
+        }
+
+        static void Add(int Y, int X)
+        {
+            int[] dY = { -1, 0, 1, 0 };
+            int[] dX = { 0, -1, 0, 1 };
+
+            int[,] map =
+            {
+                { 1, 1, 1 },
+                { 1, 1, 1 },
+                { 1, 1, 1 }
+            };
+
+            
+            
+            // y x 의 값을 받는다 
+            // 그리고 그 값에 상하좌우에 값을 더해줘야된다.
+            for( int i =0; i < 4; i++)
+            {
+                // 조건 0,0 일때 위에랑 왼쪽 값이 없다 그 값일때는 더해주지 않도록 해야된다.
+                int pY = dY[i];
+                int pX = dX[i];
+                if(Y-pY >=0 && X-pX >=0 && Y-pY <=2 && X-pX <=2)
+                {
+                    map[Y - pY, X - pX]++;
+                }
+                
+            }
+
+            for(int j =0; j<map.GetLength(0); j++)
+            {
+                for(int k =0;k<map.GetLength(1); k++)
+                {
+                    Console.Write(map[j,k]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
--- 20250911/20250911/obj/Debug/net9.0/20250911.AssemblyInfo.cs
+++ 20250911/20250911/obj/Debug/net9.0/20250911.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("20250911")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d44584a62fd394a43094005501b20d30a60c2aa8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0760fd9e164f5251d49b866a83afe94369956a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("20250911")]
 [assembly: System.Reflection.AssemblyTitleAttribute("20250911")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
